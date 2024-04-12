e213f67b8344be4cd51370bae90fff1a1c641cc9
Markos Papanikoloudis <markos.papanikoloudis@prodyna.com>
2024-04-12 08:44:11 +0200

    main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { RadioButtonModule } from 'primeng/radiobutton';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [
@@ -10,7 +22,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    TableModule,
+    HttpClientModule,
+    InputTextModule,
+    DialogModule,
+    ConfirmDialogModule,
+    InputNumberModule,
+    InputTextareaModule,
+    RadioButtonModule,
+    DropdownModule,
+    ButtonModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
